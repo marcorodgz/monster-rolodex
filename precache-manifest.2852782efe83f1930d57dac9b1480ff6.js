568ca7ffb5a760b82441bac0c816b9627a77ea82
Marco Rodriguez <cap_marc@hotmail.com>
2020-05-18 20:56:28 -0500

    Updates

---
+++ precache-manifest.2852782efe83f1930d57dac9b1480ff6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60866db0cfd473c9552630dd3a2c9ead",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "b2ff8c23cf08449c7d6c",
+    "url": "/monster-rolodex/static/css/main.427b936e.chunk.css"
+  },
+  {
+    "revision": "0105f5fa7a131123a102",
+    "url": "/monster-rolodex/static/js/2.8a77c072.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.8a77c072.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2ff8c23cf08449c7d6c",
+    "url": "/monster-rolodex/static/js/main.0a43ce6e.chunk.js"
+  },
+  {
+    "revision": "753b30f80137f55e537c",
+    "url": "/monster-rolodex/static/js/runtime-main.e7a3ea5b.js"
+  }
+]);
